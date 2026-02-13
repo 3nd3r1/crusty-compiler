e25f3d84c13b662f8e511ef16995f8983a86738e
3nd3r1 <ender.spam.guru@gmail.com>
2026-02-13 13:59:53 +0200

    feat(interpreter): implement blocks and var declarations

--- src/compiler/interpreter.rs
+++ src/compiler/interpreter.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, rc::Rc};
+
 use crate::compiler::ast;
 
 #[derive(Debug, Clone, PartialEq)]
@@ -7,14 +9,20 @@ pub enum Value {
     None,
 }
 
-pub fn interpret(node: ast::Expression) -> Result<Value, String> {
-    match node.kind {
+#[derive(Debug, Clone, PartialEq)]
+pub struct SymTab {
+    pub locals: HashMap<String, Value>,
+    pub parent: Option<Rc<SymTab>>,
+}
+
+pub fn interpret(node: &ast::Expression, symtab: &mut SymTab) -> Result<Value, String> {
+    match &node.kind {
         ast::ExpressionKind::NoneLiteral { .. } => Ok(Value::None),
-        ast::ExpressionKind::IntLiteral { value } => Ok(Value::Int(value)),
-        ast::ExpressionKind::BoolLiteral { value } => Ok(Value::Bool(value)),
+        ast::ExpressionKind::IntLiteral { value } => Ok(Value::Int(*value)),
+        ast::ExpressionKind::BoolLiteral { value } => Ok(Value::Bool(*value)),
         ast::ExpressionKind::BinaryOp { left, right, op } => {
-            let a = interpret(*left)?;
-            let b = interpret(*right)?;
+            let a = interpret(&*left, symtab)?;
+            let b = interpret(&*right, symtab)?;
             match (a, b, op) {
                 (Value::Int(a), Value::Int(b), ast::Operation::Addition) => Ok(Value::Int(a + b)),
                 (_, _, op) => Err(format!("unexpected operation {:?}", op)),
@@ -25,11 +33,33 @@ pub fn interpret(node: ast::Expression) -> Result<Value, String> {
             then_expression,
             else_expression,
         } => {
-            let cond = interpret(*condition)?;
+            let cond = interpret(&*condition, symtab)?;
             match (cond, then_expression, else_expression) {
-                (Value::Bool(true), then_expression, _) => interpret(*then_expression),
-                (Value::Bool(false), _, Some(else_expression)) => interpret(*else_expression),
+                (Value::Bool(true), then_expression, _) => interpret(&*then_expression, symtab),
+                (Value::Bool(false), _, Some(else_expression)) => {
+                    interpret(&*else_expression, symtab)
+                }
                 (cond, _, _) => Err(format!("unexpected condition {:?}", cond)),
+            }
+        }
+        ast::ExpressionKind::VarDeclaration { name, value } => {
+            let value = interpret(&*value, symtab)?;
+            symtab.locals.insert(name.clone(), value);
+            Ok(Value::None)
+        }
+        ast::ExpressionKind::Block { expressions } => {
+            let mut block_symtab = SymTab {
+                locals: HashMap::new(),
+                parent: Some(Rc::new(symtab.clone())),
+            };
+
+            if let Some((last, expressions)) = expressions.split_last() {
+                for expression in expressions {
+                    interpret(expression, &mut block_symtab)?;
+                }
+                interpret(last, symtab)
+            } else {
+                Ok(Value::None)
             }
         }
         kind => Err(format!("unexpected expression {:?}", kind)),
@@ -41,8 +71,18 @@ mod tests {
     use super::*;
     use crate::compiler::parser::tests::*;
 
+    fn empty_symtab() -> SymTab {
+        SymTab {
+            locals: HashMap::new(),
+            parent: None,
+        }
+    }
+
     #[test]
     fn test_interpreter_basics() {
-        assert_eq!(interpret(eadd(eint(2), eint(3))).unwrap(), Value::Int(5));
+        assert_eq!(
+            interpret(&eadd(eint(2), eint(3)), &mut empty_symtab()).unwrap(),
+            Value::Int(5)
+        );
     }
 }
