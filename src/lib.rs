86835025381dd8b1d62dccf94279e41b25ece4f6
3nd3r1 <ender.spam.guru@gmail.com>
2026-03-11 09:09:15 +0200

    feat(lib): Use the new module and function_irs in compile

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,10 @@ pub mod compiler;
 pub fn compile(source_code: &str, workdir: Option<&Path>) -> Result<Vec<u8>, String> {
     use crate::compiler::{assembly_generator, ir_generator, parser, tokenizer, type_checker};
     let tokens = tokenizer::tokenize(source_code)?;
-    let mut expression = parser::parse(tokens)?;
-    type_checker::typecheck(&mut expression)?;
-    let instructions = ir_generator::generate_ir(&mut expression)?;
-    let assembly_code = assembly_generator::generate_assembly(instructions)?;
+    let mut module = parser::parse(tokens)?;
+    type_checker::typecheck(&mut module)?;
+    let function_irs = ir_generator::generate_ir(&mut module)?;
+    let assembly_code = assembly_generator::generate_assembly(function_irs)?;
 
     assembler::assemble_and_get_executable(&assembly_code, workdir)
 }
