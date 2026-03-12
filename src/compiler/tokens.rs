4c749c25e3b701cdf1cc89d8aab72f54aa39f8e9
3nd3r1 <ender.spam.guru@gmail.com>
2026-03-12 14:27:41 +0200

    refactor(tokens): add Display for TokenKind and Token

--- src/compiler/tokens.rs
+++ src/compiler/tokens.rs
@@ -11,9 +11,30 @@ pub enum TokenKind {
     End,
 }
 
+impl std::fmt::Display for TokenKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let s = match self {
+            TokenKind::Keyword => "Keyword",
+            TokenKind::IntLiteral => "IntLiteral",
+            TokenKind::BoolLiteral => "BoolLiteral",
+            TokenKind::Operator => "Operator",
+            TokenKind::Identifier => "Identifier",
+            TokenKind::Punctuation => "Punctuation",
+            TokenKind::End => "End",
+        };
+        write!(f, "{}", s)
+    }
+}
+
 #[derive(Debug, Clone, PartialEq)]
 pub struct Token {
     pub kind: TokenKind,
     pub text: String,
     pub loc: Location,
+}
+
+impl std::fmt::Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}({:?})", self.kind, self.text)
+    }
 }
